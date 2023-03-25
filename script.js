65718bf2185093f354b534d00967cff0b0b3a41a
Dhiraj jaware <dhiraj.javare@gmail.com>
2023-03-25 08:02:36 +0530

    add copital feature ans styles

--- script.js
+++ script.js
@@ -8,14 +8,19 @@ btnConvert.addEventListener('click', function () {
   const input = inputText.value;
 
   if (option.value === 'uppercase' && input) {
-    const upper = input.toUpperCase();
-    inputText.value = upper;
+
+    inputText.value = input.toUpperCase();
+
   } else if (option.value === 'lowercase' && input) {
-    const lower = input.toLowerCase();
-    inputText.value = lower;
-  } else if (open.value === 'captilize'){
-  }
-  else {
+
+    inputText.value = input.toLowerCase();
+
+  } else if (option.value === 'captilize') {
+    inputText.value = input
+      .split(' ')
+      .map(w => w.replace(w[0], w[0].toUpperCase()))
+      .join(' ');
+  } else {
     alert('Please enter valid input!');
   }
 });
