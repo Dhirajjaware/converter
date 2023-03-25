2176e55ccd48de1967a11f58543123efcb9bd7d5
Dhiraj jaware <dhiraj.javare@gmail.com>
2023-03-25 08:34:07 +0530

    increase rows of textarea

--- script.js
+++ script.js
@@ -2,20 +2,21 @@
 
 const btnConvert = document.querySelector('.btn__convert');
 const inputText = document.querySelector('.input__text');
-const option = document.querySelector('.input__option');
+const inputOption = document.querySelector('.input__option');
 
 btnConvert.addEventListener('click', function () {
   const input = inputText.value;
+  const option = inputOption.value;
 
-  if (option.value === 'uppercase' && input) {
+  if (option === 'uppercase' && input) {
 
     inputText.value = input.toUpperCase();
 
-  } else if (option.value === 'lowercase' && input) {
+  } else if (option === 'lowercase' && input) {
 
     inputText.value = input.toLowerCase();
 
-  } else if (option.value === 'captilize') {
+  } else if (option === 'captilize') {
     inputText.value = input
       .split(' ')
       .map(w => w.replace(w[0], w[0].toUpperCase()))
@@ -24,3 +25,4 @@ btnConvert.addEventListener('click', function () {
     alert('Please enter valid input!');
   }
 });
+
